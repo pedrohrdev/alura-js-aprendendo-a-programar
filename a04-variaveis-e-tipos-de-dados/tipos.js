cbfdac7b21580b428cc43875da7f8759f8f6927d
pedrohrdev <pedrohrdev@gmail.com>
2026-07-31 11:11:37 -0300

    Adcionando aula sobre tipos de dados

---
+++ a04-variaveis-e-tipos-de-dados/tipos.js
@@ -0,0 +1,11 @@
+let nome = "Pedro";
+let idade = 16;
+let altura = 1.78;
+let jaTrabalha = false;
+
+console.log(`Nome: ${nome}, \n Idade: ${idade}, \n Empregado: ${jaTrabalha}`);
+
+console.log(typeof(jaTrabalha));
+
+let salario; // Undefined, nao faz sentido ter salario, pois eu nem trabalho
+let mesada = null; // Null faz sentido, pois eu ganho mesada. Null significa ausencia devalor, logo existe pois esta sem valor

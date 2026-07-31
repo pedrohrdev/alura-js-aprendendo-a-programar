fa9237c0a33fb3b08950e612bb0e3d06511df623
pedrohrdev <pedrohrdev@gmail.com>
2026-07-31 18:46:16 -0300

    Adcionando exercicio 04 aos exercicios sobre variaveis e tipos de dados

---
+++ a04-variaveis-e-tipos-de-dados/exercicios/ex8.js
@@ -0,0 +1,22 @@
+/*
+8. Contador de cliques:
+
+Simule um contador de cliques em um botão. Crie uma
+variável chamada cliquese, a cada "simulação de clique",
+atualize seu valor atribuindo o novo número
+diretamente. Mostre o total após 3 simulações.
+*/
+
+let cliques = 0;
+
+++cliques;
+console.log(`Cliques: ${cliques}`);
+
+++cliques;
+console.log(`Cliques: ${cliques}`);
+
+++cliques;
+console.log(`Cliques: ${cliques}`);
+
+++cliques;
+console.log(`O botao foi clicado ${cliques} vezes`);

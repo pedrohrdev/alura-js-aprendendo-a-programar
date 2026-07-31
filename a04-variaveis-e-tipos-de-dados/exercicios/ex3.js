fc7773db7f3db777b8f79573c879e05a92195edd
pedrohrdev <pedrohrdev@gmail.com>
2026-07-31 12:06:32 -0300

    Adcionando exercicio 03 aos exercicios sobre variaveis e tipos de dados

---
+++ a04-variaveis-e-tipos-de-dados/exercicios/ex3.js
@@ -0,0 +1,12 @@
+/*
+3. Mensagem de localização
+Crie três variáveis com os dados da sua localização: cidade,
+estado e pais. Em seguida, mostre no console:
+"Você está em Cidade - Estado, País."
+*/
+
+let suaCidadeAtual = "Blumenau";
+let seuEstadoAtual = "Santa Catarina";
+let seuPaisAtual = "Brazil";
+
+console.log(`Você está em ${suaCidadeAtual} - ${seuEstadoAtual} - ${seuPaisAtual}`)

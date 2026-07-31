a2701c7cbeb84b97363c689fec8bd7e2d83b351a
pedrohrdev <pedrohrdev@gmail.com>
2026-07-31 11:50:18 -0300

    Aula sobre legibilidade de variaveis

---
+++ a04-variaveis-e-tipos-de-dados/legibilidade.js
@@ -0,0 +1,7 @@
+// Escrevemos codigo pra pessoas, logo temos que dar bons nomes
+
+let x = 1.78 // Eu sei q isso e minha altura porem alguem que veio sem contexto isso ta ruim;
+
+let alturaDoPedro = 1.78; // Agora ficou bom, ta legivel
+//  ------D-P---- <- Letras maiusculas fazem diferenca
+console.log(alturaDopedro) // Nao vai funcionar pois n esta igual a variavel alturaDoPedro, com maiusculas no 'D' e no 'P', isso se chama case sensitive

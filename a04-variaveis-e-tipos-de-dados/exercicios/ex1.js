ffd1eac6cab6d531ad073f9ffb566f088a919c53
pedrohrdev <pedrohrdev@gmail.com>
2026-07-31 11:56:30 -0300

    Acionando exercicio 01 aos exercicios sobre variaveis e tipos de dados

---
+++ a04-variaveis-e-tipos-de-dados/exercicios/ex1.js
@@ -0,0 +1,12 @@
+/*
+1. Boas-vindas personalizadas
+
+Crie uma variável chamada nome e atribua seu nome
+completo. Depois, use console.log() para exibir a
+mensagem: "Olá, [nome]! Seja bem-vindo(a) ao curso
+de JavaScript!"
+*/
+
+const nomeCompleto = 'Pedro Henrique Rossi dos Santos';
+
+console.log(`Olá, ${nomeCompleto}! Seja bem-vindo ao curso de Javascript!`)

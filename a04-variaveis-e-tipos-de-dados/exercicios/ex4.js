d5b93a3802fa3e85435263071b227222538ececa
pedrohrdev <pedrohrdev@gmail.com>
2026-07-31 12:08:05 -0300

    Adcionando exercicio 04 aos exercicios sobre variaves e tipos de dados

---
+++ a04-variaveis-e-tipos-de-dados/exercicios/ex4.js
@@ -0,0 +1,9 @@
+/*
+4. Tipo da variável:
+Crie uma variável chamada temCarteira com valor
+true ou false. Use console.log(typeof temCarteira)
+para mostrar o tipo dessa variável.
+*/
+
+let temCarteira = false;
+console.log(typeof(temCarteira));

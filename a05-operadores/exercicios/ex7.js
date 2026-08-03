fdd74a310b2c05643a48e5dd7f683b58da285010
pedrohrdev <pedrohrdev@gmail.com>
2026-08-02 21:20:09 -0300

    Adcionando exercicio 7 aos exercicios de operadores

---
+++ a05-operadores/exercicios/ex7.js
@@ -0,0 +1,10 @@
+/*
+7. Valor negado
+Crie uma variável booleana chamada disponível. Mostre
+seu valor negado no console, ou seja, se estiver
+disponível, exiba que não está, e vice-versa.
+*/
+
+const disponivel = true;
+
+console.log(!disponivel)

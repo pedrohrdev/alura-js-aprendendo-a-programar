c4ae939e0c2a187d8f27ab234f933c0ec89e412c
pedrohrdev <pedrohrdev@gmail.com>
2026-08-03 09:35:03 -0300

    Adcionando exercicio 7 aos exercicios sobre condicionais

---
+++ a06-condicoes-e-fluxos/exercicios/ex7.js
@@ -0,0 +1,15 @@
+/*
+7. Validação de campo obrigatório
+
+Crie uma variável email. Se estiver vazia (""),
+mostre "Preencha o campo de e-mail". Caso
+contrário, mostre "E-mail válido".
+*/
+
+const email = '';
+
+if(email) {
+    console.log('E-mail valido');
+} else {
+    console.log('Preencha o campo de e-mail')
+}

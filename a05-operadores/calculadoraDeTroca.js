24b7eb50c9894962fc607e7cd66575c21e7dd51c
pedrohrdev <pedrohrdev@gmail.com>
2026-08-02 21:00:02 -0300

    Adcionando exercicio sobre operadores, onde foi implementado uma calculadora de troca

---
+++ a05-operadores/calculadoraDeTroca.js
@@ -0,0 +1,12 @@
+// O objetivo é programar uma simples calculadora de trocos
+
+let valorCompra = 20.50;
+let valorPago = 22;
+
+let troco = valorPago - valorCompra;
+
+console.log(`O valor da compra foi ${valorCompra}$ e voce pagou ${valorPago}$ \n`);
+
+console.log(`${valorPago}$ - ${valorCompra}$: ${valorPago - valorCompra}$ \n`);
+
+console.log(`Logo seu troco é de ${troco}R$`)

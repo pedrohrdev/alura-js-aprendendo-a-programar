165afb76be9bf9df6b9b634e71f8675bfd58097b
pedrohrdev <pedrohrdev@gmail.com>
2026-08-02 21:24:21 -0300

    Adcionando exercicio 9 aos exercicios sobre operadores

---
+++ a05-operadores/exercicios/ex9.js
@@ -0,0 +1,9 @@
+/*
+9. Calculadora de porcentagem
+Calcule quanto é 15% de 120 usando operadores matemáticos. Mostre a frase: "15% de 120 é igual a X."
+*/
+
+const numero = 120;
+const porcentagem = numero * 0.15;
+
+console.log(`15% de ${numero} é ${porcentagem}`)

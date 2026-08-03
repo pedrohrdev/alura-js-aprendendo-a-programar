e6fbf4d2ff0a3087a1027c711cb4d7b27cd5dacb
pedrohrdev <pedrohrdev@gmail.com>
2026-08-03 08:58:11 -0300

    Adcionando exercicio 1 aos exercicios sobre condicionais

---
+++ a06-condicoes-e-fluxos/exercicios/ex1.js
@@ -0,0 +1,11 @@
+/*
+1. Verificação de idade para compra de bebida
+
+Peça a idade da pessoa em uma variável. Se for 18 ou mais,
+mostre "Pode comprar bebida alcoólica". Caso contrário,
+mostre "Venda proibida para menores de 18 anos".
+*/
+
+let idade = 1;
+let podeComprarBebida = idade >= 18 ? 'Pode comprar bebida alcoólica' : 'Venda proibida para menores de 18 anos';
+console.log(podeComprarBebida)

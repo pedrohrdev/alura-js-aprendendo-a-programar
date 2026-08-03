d0c571845746c7f2105f9f7b60c1a7950e9b5458
pedrohrdev <pedrohrdev@gmail.com>
2026-08-03 09:29:56 -0300

    Adcionando exercicio 5 aos exercicios sobre condicionais

---
+++ a06-condicoes-e-fluxos/exercicios/ex5.js
@@ -0,0 +1,11 @@
+/*
+5. Número par ou ímpar com ternário
+
+Crie uma variável numero. Use o operador ternário
+para mostrar se ele é par ou ímpar.
+*/
+
+const numero = 67;
+const parOuImpar = numero % 2 === 0 ? 'Par' : 'Impar';
+
+console.log(parOuImpar)

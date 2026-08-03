bcedbd6e2fe68c9e422259ac56f3e2a88bec55c3
pedrohrdev <pedrohrdev@gmail.com>
2026-08-03 09:37:46 -0300

    Adcionando exercicio 8 aos exercicios sobre condicionais

---
+++ a06-condicoes-e-fluxos/exercicios/ex8.js
@@ -0,0 +1,24 @@
+/*
+8. Validação de senha segura
+
+Crie duas variáveis:
+-- senha
+-- senhaValida (valor true ou false, definido manualmente)
+
+Se senhaValida for verdadeira, mostre "Senha válida".
+Caso contrário, mostre "Senha muito curta".
+
+📌 Observação:
+Considere que a validação do tamanho da senha já foi
+feita previamente e o resultado está armazenado
+na variável senhaValida.
+*/
+
+const senha = '';
+let senhaValida = false;
+
+if(senhaValida) {
+    console.log('Senha valida');
+} else {
+    console.log('Senha muito curta')
+}

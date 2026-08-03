b59357e31f48033ba558c02c27f0057315b03a9b
pedrohrdev <pedrohrdev@gmail.com>
2026-08-03 09:06:57 -0300

    Adcionando exercicio 3 aos exercicios sobre condicionais

---
+++ a06-condicoes-e-fluxos/exercicios/ex3.js
@@ -0,0 +1,15 @@
+/*
+3. Verificação de número positivo ou negativo
+Crie uma variável com um número qualquer. Mostre
+se ele é positivo, negativo ou igual a zero.
+*/;
+
+const numero = -1;
+
+if(numero > 0) {
+    console.log(`${numero} é positivo`)
+} else if (numero === 0) {
+    console.log(`${numero} é igual á 0`)
+} else {
+    console.log(`${numero} é negativo`)
+}

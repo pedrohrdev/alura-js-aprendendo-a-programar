6db4e42436bf3fe9de1af4e5c30c120847a91d8d
pedrohrdev <pedrohrdev@gmail.com>
2026-08-04 11:13:02 -0300

    Adcionando o exercicio 4 aos exercicios sobre lacos de decisao

---
+++ a07-lacos-oporeadores-e-decisao/exercicios/ex4.js
@@ -0,0 +1,18 @@
+/*
+4. Contagem regressiva
+
+Use while para contar de 10 até 
+0 no console. Mostre uma mensagem
+no fim: "Contagem finalizada!".
+*/
+
+let dez = 11;
+
+while(dez > 0) {
+
+    console.log(dez - 1);
+
+    dez--
+
+        
+};

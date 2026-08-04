11c90d7bee11956141997abc8ce544354844f9ba
pedrohrdev <pedrohrdev@gmail.com>
2026-08-04 11:07:05 -0300

    Adcionando exercicio 2 aos exercicios sobre lacos de repeticao

---
+++ a07-lacos-oporeadores-e-decisao/exercicios/ex2.js
@@ -0,0 +1,19 @@
+/*
+
+2. Soma de 1 a 100
+
+Some todos os números de 1 a 100. Mostre
+o resultado no final com uma mensagem
+explicativa.
+
+*/
+
+let resultado = 0;
+
+for(let i = 1; i <= 100; i++) {
+
+    resultado += i;
+
+};
+
+console.log(`A soma de todos os numeros de 1 ate 100 é: ${resultado}`)

e7d3262a447ed42e174030bf3da18c1caf24172a
pedrohrdev <pedrohrdev@gmail.com>
2026-08-04 19:21:10 -0300

    Adcionando o exercicio 8 aos exercicios sobre lacos de decisao

---
+++ a07-lacos-de-decisao/exercicios/ex8.js
@@ -0,0 +1,14 @@
+/*
+8. Listando números pares
+
+Mostre todos os números pares
+entre 1 e 50 usando for.
+*/
+
+for(let i = 1; i <= 50; i++) {
+
+    if(i % 2 === 0) {
+        console.log(`Numero par: ${i}`)
+    }
+
+}

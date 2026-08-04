2b9fcc258270c953462d1c186349eeb27ced56cc
pedrohrdev <pedrohrdev@gmail.com>
2026-08-04 11:09:27 -0300

    Adcionando exercicio 3 aos exercicios sobre lacos de repeticao

---
+++ a07-lacos-oporeadores-e-decisao/exercicios/ex3.js
@@ -0,0 +1,16 @@
+/*
+3. Tabuada personalizada
+
+
+Peça um número e mostre a
+tabuada desse número (de 1 a 10)
+usando um for.
+*/
+
+const tabuadaDo = 10;
+
+for(let contador = 1; contador <= tabuadaDo; contador++) {
+
+    console.log(`${tabuadaDo} x ${contador}: ${tabuadaDo * contador}`);
+
+}

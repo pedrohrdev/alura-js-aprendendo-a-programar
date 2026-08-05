f78158ff1ae59887f201351c1495ec0abbdee77f
pedrohrdev <pedrohrdev@gmail.com>
2026-08-05 11:40:25 -0300

    Codar funcao que dobra um valor numerico

---
+++ a08-funcoes/codandoFuncaoQueDobraUmValorNumerico.js
@@ -0,0 +1,11 @@
+function calcularDobroDeUmNumero(numero) {
+
+    const dobroDoNumero = numero * 2
+
+    return dobroDoNumero;
+
+};
+
+const dobroDeDez = calcularDobroDeUmNumero(10);
+
+console.log(`O dobro de 10 é ${dobroDeDez}`)

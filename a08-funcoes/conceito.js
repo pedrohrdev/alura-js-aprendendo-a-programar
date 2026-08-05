09fbb0f12cfb42d49b8e82934b7c8b5d410d9696
pedrohrdev <pedrohrdev@gmail.com>
2026-08-05 11:22:30 -0300

    Adcionando a aula sobre funcoes

---
+++ a08-funcoes/conceito.js
@@ -0,0 +1,16 @@
+// Uma funcao e literalmente um bloco de codigo
+// executavel que tem uma funcionalidade
+
+// é como se fosse uma caixa magica, voce
+// jogar algo dentro dela(input), a caixa magica
+// faz a magica(processamento), e ela devolve
+// ou nao algo pra voce(output)
+
+// exemplo com uma funcao do primeiro grau
+
+function f(x) {
+    return x + 2;
+};
+
+console.log(f(10));
+console.log(f(2));

107aca881ac9f2d1b771a7430b32bd3d68db1648
pedrohrdev <pedrohrdev@gmail.com>
2026-08-05 10:40:22 -0300

    Adcionando o exercicio 2 aos exercicios sobre lacos de desicao

--- a07-lacos-de-decisao/exercicios/ex10.js
+++ a07-lacos-de-decisao/exercicios/ex10.js
@@ -15,3 +15,22 @@ A escolha da opção deve ser simulada por valores
 atribuídos à variável opcao.
 */
 
+let opcao = 1;
+
+do {
+
+    console.log('1 - Ver saldo');
+    console.log('2 - Fazer deposito');
+    console.log('3 - Sair');
+
+    if(opcao === 1){
+        console.log(`Opçao escolhida: ${opcao} - Ver saldo: \n Seu saldo: 249,45R$ \n`)
+    } else if(opcao === 2) {
+        console.log(`Opcao escolhida: ${opcao} - Fazer deposito: \n Voce depositou 500R$\n`)
+    } else {
+        console.log(`Opcao escolhida: ${opcao} - Sair: \n tchau!\n`)
+    }
+
+    opcao++;
+
+} while(opcao <= 3);

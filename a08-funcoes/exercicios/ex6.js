ac5ab779555f846232881e48bb134bfc7a847501
pedrohrdev <pedrohrdev@gmail.com>
2026-08-07 10:04:15 -0300

    Adcionando o exercicio 6 aos exercicios sobre funcoes

---
+++ a08-funcoes/exercicios/ex6.js
@@ -0,0 +1,14 @@
+/*
+6. Função soma
+
+Crie uma função que recebe dois números e retorna
+a soma deles. Exiba o resultado no console com uma frase completa.
+*/
+
+function sum(n1, n2) {
+    return n1 + n2;
+};
+
+console.log(`1 + 2: ${sum(1, 2)}`);
+console.log(`10 + 2: ${sum(10, 2)}`);
+console.log(`100 + 2: ${sum(100, 2)}`);

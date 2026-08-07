66d27fc061557cf33eb943c528d6bedc941857d9
pedrohrdev <pedrohrdev@gmail.com>
2026-08-07 09:52:22 -0300

    Adcionando o exercicio 1 aos exercicios sobre funcoes

---
+++ a08-funcoes/exercicios/ex1.js
@@ -0,0 +1,13 @@
+/*
+1. Função de saudação
+
+Crie uma função chamada saudacao que exibe a
+mensagem "Olá! Seja bem-vindo(a)!" ao ser chamada.
+*/
+
+// Nao vou usar um parametro nome, pois apenas pediu que eu exibisse uma mensagem, e nao retornarei nada pois denovo, apenas foi pedido que eu exibisse
+function saudacao() {
+    console.log(`Olá! seja bem-vindo`);
+};
+
+saudacao()

5127cc9720569b9b76fe29a39d9d818ce92759c6
pedrohrdev <pedrohrdev@gmail.com>
2026-08-07 09:54:03 -0300

    Adcionando o exercicio 2 aos exercicios sobre funcoes

--- a08-funcoes/exercicios/ex1.js
+++ a08-funcoes/exercicios/ex1.js
@@ -10,4 +10,4 @@ function saudacao() {
     console.log(`Olá! seja bem-vindo`);
 };
 
-saudacao()
+saudacao();
---
+++ a08-funcoes/exercicios/ex2.js
@@ -0,0 +1,12 @@
+/*
+2. Função com parâmetros
+
+Crie uma função apresentarPessoa(nome, idade) que exibe
+no console: "Olá, meu nome é [nome] e tenho [idade] anos."
+*/
+
+function apresentarPessoa(nome, idade) {
+    console.log(`Olá, meu nome é ${nome} e tenho ${idade} anos`)
+};
+
+apresentarPessoa('Pedro', 16);

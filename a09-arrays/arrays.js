1b443c13fbe203ff74e805f49f22272e5e23988a
pedrohrdev <pedrohrdev@gmail.com>
2026-08-16 16:05:59 -0300

    Aula sobre arrays

---
+++ a09-arrays/arrays.js
@@ -0,0 +1,11 @@
+// Um array é uma lista de valores guardados em uma variavel
+const nomes = ['Pedro', 'Joao', 'Lucas', 'Tiago'];
+
+// Se fossem varias variaveis com nomes distintos:
+const nome1 = 'Pedro';
+const nome2 = 'Joao';
+const nome3 = 'Lucas';
+const nome4 = 'Tiago';
+
+// fica mais pratico usando um array,
+console.log(nomes.length)

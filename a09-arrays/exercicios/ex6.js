d9de01e5d5e6ed6009027f77b603dc7cb6d8e0ca
pedrohrdev <pedrohrdev@gmail.com>
2026-08-17 08:27:07 -0300

    Adcionando o exercicio 6 aos exercicios sobre arrays

---
+++ a09-arrays/exercicios/ex6.js
@@ -0,0 +1,12 @@
+/*
+6. Mensagens personalizadas com forEach
+
+Crie um array com nomes de usuários. Use forEach
+para imprimir "Olá, [nome]!" para cada um.
+*/
+
+const nomes = ['Pedro', 'Neymar', 'Messi'];
+
+nomes.forEach((nome) => {
+    console.log(`Ola ${nome}!`);
+});

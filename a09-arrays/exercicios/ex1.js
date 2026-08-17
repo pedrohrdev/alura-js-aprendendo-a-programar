1698adca32f6679c2ba8e3e74e62d67846d5e0cb
pedrohrdev <pedrohrdev@gmail.com>
2026-08-17 08:05:51 -0300

    Adcionando o exercicio 1 aos exercicios sobre arrays

---
+++ a09-arrays/exercicios/ex1.js
@@ -0,0 +1,12 @@
+/*
+1. Lista de nomes
+
+Crie um array com 5 nomes. Use for para imprimir
+cada nome no console, um por linha.
+*/
+
+const nomes = ['Pedro', 'Benjamin', 'Gustavo', 'Theo'];
+
+for(let i = 0; i < nomes.length; i++) {
+    console.log(nomes[i]);
+}

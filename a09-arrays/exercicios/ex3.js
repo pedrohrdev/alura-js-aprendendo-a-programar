ab1ff9fff7fd4470417d815d5f306397c792baa4
pedrohrdev <pedrohrdev@gmail.com>
2026-08-17 08:10:32 -0300

    Adcionando o exercicio 3 aos exercicios sobre arrays

---
+++ a09-arrays/exercicios/ex3.js
@@ -0,0 +1,12 @@
+/*
+3. Contar itens do array
+
+Crie um array com nomes de cidades e exiba no
+console a quantidade total de itens usando .length.
+*/
+
+const cidades = ['Maringa', 'Blumenau', 'Marialva', 'Pomerode'];
+
+const quantidadeDeCidade = cidades.length;
+
+console.log(`O total de cidades no array é ${quantidadeDeCidade}`)

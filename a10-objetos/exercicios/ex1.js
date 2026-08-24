cf8d3ba080ea888cac62d777f681d3c3657122ba
pedrohrdev <pedrohrdev@gmail.com>
2026-08-24 11:45:12 -0300

    Adcionando exercicio 1 aos exercicios sobre objetos

---
+++ a10-objetos/exercicios/ex1.js
@@ -0,0 +1,9 @@
+/*
+1. Criando um objeto pessoal
+Crie um objeto com seu nome, idade e profissão.
+*/
+const pessoa = {
+    nome: "Pedro",
+    idade: 16,
+    profissao: "Desenvolvedor de Software Autonomo"
+}

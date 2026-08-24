3ac39fb7a48405540ae9e4605bf03d011af55ce2
pedrohrdev <pedrohrdev@gmail.com>
2026-08-24 11:46:36 -0300

    Adcionando o exercicio 2 aos exercicios sobre objetos

---
+++ a10-objetos/exercicios/ex2.js
@@ -0,0 +1,14 @@
+/*
+2. Acessando propriedades
+Acesse e exiba o valor da propriedade "nome" no console.
+*/
+
+const pessoa = {
+    nome: "Pedro",
+    idade: 16,
+    profissao: "Desenvolvedor de Software Autonomo"
+};
+
+const nomePessoa = pessoa.nome;
+
+console.log(nomePessoa)

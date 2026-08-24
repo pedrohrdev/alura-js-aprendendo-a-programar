92f77d306014e7b8ac9928c9422306e0e3479a1c
pedrohrdev <pedrohrdev@gmail.com>
2026-08-24 11:47:51 -0300

    Adcionando o exercicio 3 aos exercicios sobre objetos

---
+++ a10-objetos/exercicios/ex3.js
@@ -0,0 +1,15 @@
+/*
+3. Atualizando valores
+Modifique a propriedade "idade" com um novo valor.
+*/
+
+const pessoa = {
+    nome: "Pedro",
+    idade: 16,
+    profissao: "Desenvolvedor de Software Autonomo"
+};
+
+// Fez aniversario!
+pessoa.idade = 17;
+
+console.log(pessoa)

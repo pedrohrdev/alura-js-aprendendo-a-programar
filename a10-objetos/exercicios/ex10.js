97c29e603a1846772bc5607a135f7cf3335415a8
pedrohrdev <pedrohrdev@gmail.com>
2026-08-24 12:09:56 -0300

    Adcionando o exercicio 10 aos exercicios sobre objetos

---
+++ a10-objetos/exercicios/ex10.js
@@ -0,0 +1,11 @@
+/*
+10. Cálculo de compra
+Crie um objeto produto com preco e quantidade, e calcule o valor total da compra(preco * quantidade).
+*/
+
+const produto = {
+    preco: 100,
+    quantidade: 3 
+};
+
+console.log(`Valor total da compra: ${produto.quantidade} tenis, valor individual: ${produto.valor}, total da compra: ${produto.preco * produto.quantidade}`)

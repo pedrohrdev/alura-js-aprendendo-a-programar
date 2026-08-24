a4db72c346cb0b5a23a5694ca863eea069a07bb9
pedrohrdev <pedrohrdev@gmail.com>
2026-08-24 11:48:55 -0300

    Adcionando o exercicio 4 aos exercicios sobre objetos

---
+++ a10-objetos/exercicios/ex4.js
@@ -0,0 +1,14 @@
+/*
+4. Adicionando uma nova propriedade
+Adicione ao objeto uma nova propriedade chamada "cidade".
+*/
+
+const pessoa = {
+    nome: "Pedro",
+    idade: 16,
+    profissao: "Desenvolvedor de Software Autonomo"
+};
+
+pessoa.cidade = "Blumenau";
+
+console.log(pessoa)

94b7809d47fc84f1debffa19d7e8f4b963bfc87d
pedrohrdev <pedrohrdev@gmail.com>
2026-08-24 11:55:31 -0300

    Adcionando o exercicio 6 aos exercicios sobre objeto

---
+++ a10-objetos/exercicios/ex6.js
@@ -0,0 +1,21 @@
+/*
+6. Lista de pessoas
+Crie um array com 3 objetos, cada um representando uma pessoa com nome e idade.
+*/
+
+const pessoas = [
+    {
+        nome: "Pedro",
+        idade: 16
+    },
+
+    {
+        nome: "Elon Musk",
+        idade: 96
+    },
+    
+    {
+        nome: "Neymar",
+        idade: 34
+    },    
+]

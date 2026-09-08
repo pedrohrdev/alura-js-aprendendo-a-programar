bdded2cec69ae81d2e8d6165281f03974d07446c
pedrohrdev <pedrohrdev@gmail.com>
2026-09-08 10:50:58 -0300

    Resolvendo o exercicio 4 sobre fundamentos modernos

---
+++ a11-fundamentos-modernos/exercicios/ex4.js
@@ -0,0 +1,11 @@
+/*
+4. Spread operator com arrays
+Crie dois arrays de frutas e combine-os usando o operador spread.
+*/
+
+let frutas1 = ['Maça', 'Banana'];
+let frutas2 = ['Pera', 'Uva'];
+
+let totalFrutas = [...frutas1, ...frutas2];
+
+console.log(totalFrutas)

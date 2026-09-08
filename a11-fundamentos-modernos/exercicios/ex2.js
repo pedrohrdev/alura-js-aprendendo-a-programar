16c6ef0624a8fdc515de9228f2318c7e8c1d25e1
pedrohrdev <pedrohrdev@gmail.com>
2026-09-08 10:41:53 -0300

    Resolvendo o exercicio 2 sobre fundamentos modernos

---
+++ a11-fundamentos-modernos/exercicios/ex2.js
@@ -0,0 +1,11 @@
+/*
+2. Destructuring em arrays
+Crie um array com 3 linguagens de programação. Use destructuring para criar variáveis ling1, ling2 e ling3.
+*/
+
+const langs = ['JavaScript', 'Typescript', 'Golang'];
+const [ lang1, lang2, lang3 ] = langs;
+
+console.log(lang1);
+console.log(lang2);
+console.log(lang3);

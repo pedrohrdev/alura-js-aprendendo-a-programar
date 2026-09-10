e69c2bad0137c8840db2d6217e2dc5809ee09096
pedrohrdev <pedrohrdev@gmail.com>
2026-09-10 12:02:45 -0300

    Resolvendo o exercicio 5 do dia 1

---
+++ exerciciosLogica/logica/dia1/ex5.js
@@ -0,0 +1,7 @@
+// — Conversor de Temperatura
+
+const temperaturaEmCelsius = 25;
+
+const fahrenheit = (temperaturaEmCelsius * 9 / 5) + 32;
+
+console.log(`${temperaturaEmCelsius} celsius para fahrenheit sao ${fahrenheit} graus fahrenheit`)

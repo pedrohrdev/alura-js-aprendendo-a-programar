573f3d6f454b6b9faf9930fa8d9dde37bf0ebb9a
pedrohrdev <pedrohrdev@gmail.com>
2026-09-10 14:17:42 -0300

    Adcionando comentarios com o meu raciciocinio no ex 8 do dia 1

--- exerciciosLogica/logica/dia1/ex8.js
+++ exerciciosLogica/logica/dia1/ex8.js
@@ -1,34 +1,40 @@
 // Cedulas
 // 100, 50, 20, 10, 5, 2 e 1
 
+// as variaveis de cedula sao responsaveis para guardar quantas cedulas sao possiveis pagar
+// e o calculo abaixo quanto sobra pra calcular cada cedula
+
 let valor = 435;
 
-const cedulaDeCem = Math.floor(valor / 100);
-valor = valor % 100;
+// Pegando quantas notas de cem paga 400
+const nota100 = Math.floor(valor / 100);
+// Depois que pegamos as de 100 vamos calcular oque sobrou para calcular com as notas posteriores
+valor = valor % 100; // vai dar 35
+// console.log(valor)
 
-const cedulaDeCinquenta = Math.floor(valor / 50);
+// Agora valor é 35, entao ele ja pegou as de 400, agora vai ver se da pra pagar com a de 50 com o valor q sobrou(35) e assim sucesivamente..
+const nota50 = Math.floor(valor / 50); // vai dar 0 pois sobrou 35
 valor = valor % 50;
 
-const cedulaDeVinte = Math.floor(valor / 20);
+const nota20 = Math.floor(valor / 20);
 valor = valor % 20;
 
-const cedulaDeDez = Math.floor(valor / 10);
+const nota10 = Math.floor(valor / 10); // se valor for menor que dez vai dar zero entao, se for 10 pra cima da 1
 valor = valor % 10;
 
-const cedulaDeCinco = Math.floor(valor / 5);
+const nota5 = Math.floor(valor / 5);
 valor = valor % 5;
 
-const cedulaDeDois = Math.floor(valor / 2);
+const nota2 = Math.floor(valor / 2);
 valor = valor % 2;
 
-
-const cedulaDeUm = Math.floor(valor / 1);
+const nota1 = Math.floor(valor / 1);
 valor = valor % 1;
 
-console.log(`Notas de 100: ${cedulaDeCem}`);
-console.log(`Notas de 50: ${cedulaDeCinquenta}`);
-console.log(`Notas de 20: ${cedulaDeVinte}`);
-console.log(`Notas de 10: ${cedulaDeDez}`);
-console.log(`Notas de 5: ${cedulaDeCinco}`);
-console.log(`Notas de 2: ${cedulaDeDois}`);
-console.log(`Notas de 1: ${cedulaDeUm}`);
+console.log(`Notas de 100: ${nota100}`);
+console.log(`Notas de 50: ${nota50}`);
+console.log(`Notas de 20: ${nota20}`);
+console.log(`Notas de 10: ${nota10}`);
+console.log(`Notas de 5: ${nota5}`);
+console.log(`Notas de 2: ${nota2}`);
+console.log(`Notas de 1: ${nota1}`);

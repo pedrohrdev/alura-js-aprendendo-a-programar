41a512ec114a09ba765b915bddb62e4d5efbf7a3
pedrohrdev <pedrohrdev@gmail.com>
2026-09-15 10:10:19 -0300

    Resolvendo exercicio 9 do dia 23

---
+++ exerciciosLogica/logica/dia2/ex9.js
@@ -0,0 +1,8 @@
+// ## Exercício 09 — Conta do restaurante
+
+let valorConsumido = 1000;
+let quantidadeDePessoas = 10;
+let totalDaCompra = (valorConsumido * 0.10) + valorConsumido;
+let cadaUmVaiPagar = totalDaCompra / quantidadeDePessoas
+
+console.log(`Cada pessoa vai pagar ${cadaUmVaiPagar}`);

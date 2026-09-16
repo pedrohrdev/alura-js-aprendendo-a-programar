3e9bd44b46f06eaab02112f63ca1480efe534ead
pedrohrdev <pedrohrdev@gmail.com>
2026-09-16 20:44:45 -0300

    Resolvendo exercicio 4 do dia 4

---
+++ exerciciosLogica/logica/dia4/ex3.js
@@ -0,0 +1,13 @@
+function calcularMedia(n1, n2, n3) {
+
+    let soma = n1 + n2 + n3;
+
+    let mediaAritimetica = soma / 3;
+    
+
+    return mediaAritimetica
+
+};
+
+console.log(calcularMedia(10, 20, 30));
+console.log(calcularMedia(5, 7, 9));
